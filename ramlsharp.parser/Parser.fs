59158ea1aa3a76591ac4eb7fa047435190487346
Chester Husk III <chusk3@gmail.com>
2014-05-13 22:28:41 -0500

    duh, I need to chain together these top-level elements

--- ramlsharp.parser/Parser.fs
+++ ramlsharp.parser/Parser.fs
@@ -6,9 +6,9 @@ open AST
 let ws = spaces
 let str s = pstring s
 
-let uri : Parser<Raml,unit> = skipString "baseUri: " >>. restOfLine true |>> BaseUri
-let ramlTitle : Parser<Raml,unit> = skipString "title: " >>. restOfLine true |>> Title
+let baseUri : Parser<Raml,unit> = skipString "baseUri: " >>. restOfLine true |>> BaseUri
+let title : Parser<Raml,unit> = skipString "title: " >>. restOfLine true |>> Title
 let ramlVer : Parser<Raml,unit> = skipString "%#RAML " >>. pfloat |>> Version
-let ramlDef = ramlVer // will incrementally expand this definition to include the rest
+let ramlDef = ramlVer .>> title .>> baseUri // will incrementally expand this definition to include the rest
 
 let raml = ws >>. ramlDef .>> ws .>> eof
