a479332f91cff27b4582f5466dbaa40afdd26bb6
Chester Husk III <chusk3@gmail.com>
2014-07-14 22:30:53 -0500

    some trivial cleanups

--- ramlsharp.parser/Parser.fs
+++ ramlsharp.parser/Parser.fs
@@ -6,14 +6,14 @@ open AST
 // PARSER HELPERS
 let ws = spaces
 let str s = pstring s
-let getAllUntil s shouldSkip = charsTillStringCI s shouldSkip System.Int32.MaxValue
+let getAllUntilNext s shouldSkip = charsTillStringCI s shouldSkip System.Int32.MaxValue
 let advanceToEOL = restOfLine true
 let ThisOrEndOfLine parser = parser <|> advanceToEOL
 
 // PARAMETERS, ROUTE AND QUERY STRING
-let paramParser = str "{" >>. ThisOrEndOfLine (getAllUntil "}" true) |>> Parameter.Undetermined
+let paramParser = str "{" >>. ThisOrEndOfLine (getAllUntilNext "}" true) |>> Parameter.Undetermined
 let routeParamParser = paramParser
-let staticRouteParser = ThisOrEndOfLine (getAllUntil "/" true) // either go until a slash or, if we're at the end of the line, grab the rest of the line
+let staticRouteParser = ThisOrEndOfLine (getAllUntilNext "/" true) // either go until a slash or, if we're at the end of the line, grab the rest of the line
 
 // PROTOCOLS
 let httpParser : Parser<Protocol,unit> =  str "http" |>> fun _ -> Protocol.Http
@@ -36,18 +36,18 @@ let makeUri p d = // statics routeparameters = //TODO route params....
 
 let domainParser = str "://" >>. many1Satisfy (fun c -> (not (c = '/') && not (c = '\n')))
 // TODO: need to account for static routes, like <domain>/api/{param}. we don't handle the /api yet...
-let baseUri = skipString "baseUri: " >>. pipe2 protocolParser domainParser makeUri 
+let baseUri = skipString "baseUri:" >>. ws >>. pipe2 protocolParser domainParser makeUri 
 
 // SIMPLE RAML PROPERTIES
-let title : Parser<string,unit> = skipString "title: " >>. advanceToEOL
-let ramlVer : Parser<float,unit> = skipString "#%RAML " >>. pfloat .>> advanceToEOL
+let title = skipString "title:" >>. ws >>. advanceToEOL
+let versionSentinel = skipString "#%RAML"
+let ramlVer = versionSentinel .>> ws >>. pfloat .>> advanceToEOL
 
-let makeObj version title uri = 
-    printfn "making raml with version %f and title %s" version title
+let makeRamlDef version title uri = 
     {
-        title = title
         version = version
+        title = title
         baseUri = uri
     }
 
-let raml = pipe3 ramlVer title baseUri makeObj
+let raml = pipe3 ramlVer title baseUri makeRamlDef
